6b27228fb25916439a5942f6fe4e7fea45e43903
Feasy01 <szymon.walczak06@gmail.com>
2022-05-24 02:43:01 +0100

    make it better

--- client/src/pages/Onboarding.jsx
+++ client/src/pages/Onboarding.jsx
@@ -25,7 +25,7 @@ function Onboarding() {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const response = await axios.put("http://localhost:8000/user", {
+			const response = await axios.put("/user", {
 				formData,
 			});
 			const success = response.status === 200;
